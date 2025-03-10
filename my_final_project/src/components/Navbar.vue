1716a62f5432b6d4463f256e13081dac04c93f1d
Maria Theresa Carreon <122862523+theacarreon@users.noreply.github.com>
2025-03-10 22:57:40 +0800

    added navbar logo

--- my_final_project/src/components/Navbar.vue
+++ my_final_project/src/components/Navbar.vue
@@ -2,7 +2,7 @@
     <nav class="navbar navbar-expand-sm fixed-top">
       <div class="container">
         <a class="navbar-brand" href="#title-welcome">
-          <img :src="https://clipground.com/images/wicked-musical-clipart-1.png" alt="home page">
+          <img :src="wickedLogo" alt="home page">
         </a>
         <ul class="navbar-nav me-auto d-flex align-items-center">
           <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
@@ -12,3 +12,7 @@
       </div>
     </nav>
   </template>
+
+<script setup>
+import wickedLogo from '@/assets/images/wicked-logo.png'; 
+</script>
