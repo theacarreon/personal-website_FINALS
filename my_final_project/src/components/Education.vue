e5205305833be996f255b44caa4c9f6e33956bb5
Maria Theresa Carreon <122862523+theacarreon@users.noreply.github.com>
2025-03-10 21:44:50 +0800

    added images

--- my_final_project/src/components/Education.vue
+++ my_final_project/src/components/Education.vue
@@ -10,31 +10,39 @@
       <tr>
         <td>2022 - Present</td>
         <td>
-          <img src="http://apc.edu20.org/files/22095/Asia_Pacific_College_Logo(2).png?lmsauth=7dc65e3ec98d480e996b6f9108ffe7ed895b28fb" alt="APC Logo" class="school-logo">
+          <img src="image1" alt="APC Logo" class="school-logo">
           NU - Asia Pacific College</td>
         <td>Bachelor of Science in Computer Science <br> with Specialization in Software Systems</td>
       </tr>
       <tr>
         <td>2014 - 2018</td>
         <td>
-          <img src="https://facultyportal.cvsu-naic.edu.ph/cvsu-logo.png" alt="cvsu logo" class="school-logo">
+          <img src="image2" alt="cvsu logo" class="school-logo">
          Cavite State University - Imus Campus</td>
         <td>Bachelor of Arts in Mass Communication <br> Major in Journalism</td>
       </tr>
       <tr>
         <td>2010 - 2014</td>
         <td>
-          <img src="https://scontent.fmnl4-6.fna.fbcdn.net/v/t39.30808-6/309102315_490442153097469_8205128745794884935_n.png?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeGLVIIb6nEefRsTPOOaeCJjQaHwj13_6kBBofCPXf_qQGsgBMrPDDScQhGNSkO9gF18NO0j16ItX26e5HkotKRY&_nc_ohc=GdnHVRYdfWAQ7kNvgFRD35Z&_nc_oc=AdisleikABTcG7JXkQCKJw2-U05FRSqfdq5FRdNoyZBCyCkiW8WbW4ZsVx3vwkSsFCC6UfPtkCAO8aXp281z_ctH&_nc_zt=23&_nc_ht=scontent.fmnl4-6.fna&_nc_gid=AORYTx7byqRTCbtTBWwKs15&oh=00_AYAuu8LUfLxRXcbYlqdywTT_hzCNSHwjVWHaR1HDv3v8Xw&oe=67CB4175" alt="bnhs logo" class="school-logo">
+          <img src="image3" alt="bnhs logo" class="school-logo">
          Bacoor National High School - Villa Maria Annex</td>
         <td>High School</td>
       </tr>
       <tr>
         <td>2004 - 2010</td>
         <td>
-          <img src="https://tse2.mm.bing.net/th?id=OIP.UcfHA59eRhIP668reUBUNgAAAA&pid=Api&P=0&h=180" alt="cca logo" class="school-logo">
+          <img src="image4" alt="cca logo" class="school-logo">
          Citiville Christian Academy</td>
         <td>Elementary</td>
       </tr>
       </table>
     </section>
 </template>
+
+
+<script setup>
+import image1 from '@/assets/images/APC Logo.png';
+import image2 from '@/assets/images/cvsu-logo.jpg';
+import image3 from '@/assets/images/bnhs-logo.jpg';
+import image4 from '@/assets/images/cca-logo.jpg';
+</script>
