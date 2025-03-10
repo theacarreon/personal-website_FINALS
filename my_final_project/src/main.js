525f1e6a558333332298438e0214a91f5a078b2d
Maria Theresa Carreon <122862523+theacarreon@users.noreply.github.com>
2025-03-10 13:57:23 +0000

    changes in main.js

--- my_final_project/src/main.js
+++ my_final_project/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import './assets/mystyles.css';
+import 'assets/mystyles.css';
 
 const app = createApp(App);
 app.mount('#app');
