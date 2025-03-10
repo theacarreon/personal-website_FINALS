a7ce799a96d9169d4685a936393f3ad4ce1c224a
Maria Theresa Carreon <122862523+theacarreon@users.noreply.github.com>
2025-03-10 20:53:43 +0800

    transferred html to vue.js

--- my_final_project/src/main.js
+++ my_final_project/src/main.js
@@ -1,6 +1,8 @@
-import './assets/main.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './assets/mystyles.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+const app = createApp(App);
+app.mount('#app');
